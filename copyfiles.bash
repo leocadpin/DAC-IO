6e4a35affc3eea5988122867c5f11b8d2efc5bbd
leocadpin <leopoldocadavid@gmail.com>
2026-01-21 22:02:08 +0100

    first commit

---
+++ copyfiles.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ruta origen (tu proyecto STM32)
+DESTINO="/home/leo/Documentos/DAC-IO/stm32/DAC-IO-STM32
+"
+
+# Ruta destino (tu carpeta dentro del proyecto)
+ORIGEN="/home/leo/STM32CubeIDE/workspace_1.15.0/CANBUSTESTING"
+
+# Crear destino si no existe
+mkdir -p "$DESTINO"
+
+# Copiar archivos clave
+cp -v "$ORIGEN"/*.ioc "$DESTINO" 2>/dev/null
+cp -v "$ORIGEN"/.project "$DESTINO" 2>/dev/null
+cp -v "$ORIGEN"/.cproject "$DESTINO" 2>/dev/null
+cp -v "$ORIGEN"/.stm32cubemx "$DESTINO" 2>/dev/null
+
+# Copiar carpetas clave
+cp -rv "$ORIGEN"/Core "$DESTINO" 2>/dev/null
+cp -rv "$ORIGEN"/Drivers "$DESTINO" 2>/dev/null
+cp -rv "$ORIGEN"/Middlewares "$DESTINO" 2>/dev/null
+
+echo "Copia completa. Revisa la carpeta destino."
